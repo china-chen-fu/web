1106bae08bd750e38061e6df1f996c0752bb31e2
chenzhang <245118190@qq.com>
2023-10-09 18:46:33 +0800

    xmind

--- src/router/index.js
+++ src/router/index.js
@@ -28,27 +28,6 @@ import Layout from "@/layout";
 // 公共路由
 export const constantRoutes = [
   {
-    path: "/redirect",
-    component: Layout,
-    hidden: true,
-    children: [
-      {
-        path: "/redirect/:path(.*)",
-        component: () => import("@/views/redirect/index.vue"),
-      },
-    ],
-  },
-  {
-    path: "/login",
-    component: () => import("@/views/login"),
-    hidden: true,
-  },
-  {
-    path: "/register",
-    component: () => import("@/views/register"),
-    hidden: true,
-  },
-  {
     path: "/:pathMatch(.*)*",
     component: () => import("@/views/error/404"),
     hidden: true,
@@ -57,20 +36,6 @@ export const constantRoutes = [
     path: "/401",
     component: () => import("@/views/error/401"),
     hidden: true,
-  },
-  {
-    path: "/user",
-    component: Layout,
-    hidden: true,
-    redirect: "noredirect",
-    children: [
-      {
-        path: "profile",
-        component: () => import("@/views/system/user/profile/index"),
-        name: "Profile",
-        meta: { title: "个人中心", icon: "user" },
-      },
-    ],
   },
 ];
 
--- src/views/project/yunboo/mindMap/index.vue
+++ src/views/project/yunboo/mindMap/index.vue
@@ -1,9 +1,123 @@
 <script setup>
+/**
+  *作者：陈章
+  *时间：2023/10/09 13:59:06
+  *功能：实现一个简单的脑图结构
+  */
+import {SVG, Text} from '@svgdotjs/svg.js'
+
+
+class Node {
+  //构造函数
+  constructor(opt = {}) {
+    this.nodeData = opt.data// 节点真实数据，就是上述说的渲染树的节点
+    this.isRoot =  opt.isRoot// 是否是根节点
+    this.layerIndex = opt.layerIndex// 节点层级
+    this.width = 0// 节点宽
+    this.height = 0// 节点高
+    this.left = opt.left || 0// left
+    this.top = opt.top || 0// top
+    this.parent = opt.parent || null// 父节点
+    this.children = []// 子节点
+    // ...
+
+  }
+
+  //计算节点宽高
+  getSize(){
+    let textData = this.createTextNode()
+    this.width = textData.width + 20
+    this.height = textData.height + 10
+  }
+
+  createTextNode(){
+    let node = new Text().text(this.nodeData.data.text)
+    let { width , height } =node.bbox()
+    return {
+      node,
+      width,
+      height
+    }
+  }
+
+  render(){
+    let textData = this.createTextNode()
+
+    textData.node.x(10).y(5)
+
+    this.group.rect(this.width,this.height).x(0).y(0)
+
+    this.group.add(textData.node)
+
+    this.group.translate(this.left,this.right)
+
+    this.draw.add(this.group)
+  }
+
+
+}
+
+// class Render {
+//   // 第一次遍历渲染树
+//   walk(this.renderer.renderTree, null, (cur, parent, isRoot, layerIndex) => {
+//   // 先序遍历
+//   // 创建节点实例
+//   let newNode = new Node({
+//     data: cur,// 节点数据
+//     layerIndex// 层级
+//   })
+//   // 节点实例关联到节点数据上
+//   cur._node = newNode
+//   // 根节点
+//   if (isRoot) {
+//     this.root = newNode
+//     // 定位在画布中心位置
+//     newNode.left = (this.mindMap.width - node.width) / 2
+//     newNode.top = (this.mindMap.height - node.height) / 2
+//   } else {// 非根节点
+//   // 互相收集
+//   newNode.parent = parent._node
+//   parent._node.addChildren(newNode)
+//   // 定位到父节点右侧
+//   newNode.left = parent._node.left + parent._node.width + marginX
+// }
+// }, null, true, 0)
+// }
+
+onMounted(()=>{
+  const draw = SVG().addTo('.svg').size(600, 600)
+  const rect = draw.rect(600, 600).attr({ fill: "#ccc" })
+  let group = draw.group()
+  let node = new Node({
+    data:{
+      data:{
+        text:'我是节点'
+      }
+    }
+  })
+  node.group = group
+  node.draw = draw
+  node.render()
+})
+
+
+/**
+  *作者：陈章
+  *时间：2023/10/09 17:44:27
+  *功能：是指一个且套结构可以渲染node节点
+  */
+
+
+
+
+
 
 </script>
 
 <template>
-<div>脑图</div>
+<div class="svg">
+
+</div>
 </template>
 
 <style scoped lang="scss">
