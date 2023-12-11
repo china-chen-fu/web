a906c98522e69df4b97cc1e4b41044969746401b
chenzhang <245118190@qq.com>
2023-12-11 19:05:48 +0800

    the function of json

--- src/data/routes/index.js
+++ src/data/routes/index.js
@@ -1,171 +1,185 @@
-export const pageRoutes={
-    "code": 200,
-    "msg": "操作成功",
-    "type": 0,
-    "data": [
+export const pageRoutes = {
+  code: 200,
+  msg: '操作成功',
+  type: 0,
+  data: [
+    {
+      path: '/',
+      hidden: false,
+      component: 'Layout',
+      children: [
         {
-            "path": "/",
-            "hidden": false,
-            "component": "Layout",
-            "children": [
-                {
-                    "name": "Home",
-                    "path": "home",
-                    "hidden": false,
-                    "component": "index",
-                    "meta": {
-                        "title": "首页",
-                        "englishName": "",
-                        "icon": "energy-dn-kanban",
-                        "noCache": false,
-                        "link": null
-                    },
-                    "platformId": 1
-                }
-            ],
-            "platformId": 1
-        },
-        {
-            "name": "Project",
-            "path": "/project",
-            "hidden": false,
-            "redirect": "noRedirect",
-            "component": "Layout",
-            "alwaysShow": true,
-            "meta": {
-                "title": "项目DEMO",
-                "englishName": "",
-                "icon": "energy-dn-houtai",
-                "noCache": false,
-                "link": null
-            },
-            "children": [
-                {
-                    "name": "Yunboo",
-                    "path": "yunboo",
-                    "hidden": false,
-                    "redirect": "noRedirect",
-                    "component": "ParentView",
-                    "alwaysShow": true,
-                    "meta": {
-                        "title": "鼋博",
-                        "englishName": null,
-                        "icon": "",
-                        "noCache": false,
-                        "link": null
-                    },
-                    "children": [
-                        {
-                            "name": "MindMap",
-                            "path": "mindMap",
-                            "hidden": false,
-                            "component": "project/yunboo/mindMap/index",
-                            "meta": {
-                                "title": "脑图",
-                                "englishName": null,
-                                "icon": "",
-                                "noCache": false,
-                                "link": null
-                            },
-                            "platformId": 1
-                        },
-                        {
-                            "name": "GaussianDistribution",
-                            "path": "gaussianDistribution",
-                            "hidden": false,
-                            "component": "project/yunboo/gaussianDistribution/index",
-                            "meta": {
-                                "title": "正态分布",
-                                "englishName": null,
-                                "icon": "",
-                                "noCache": false,
-                                "link": null
-                            },
-                            "platformId": 1
-                        },
-                        {
-                            "name": "MassData",
-                            "path": "massData",
-                            "hidden": false,
-                            "component": "project/yunboo/massData/index",
-                            "meta": {
-                                "title": "表格大量数据",
-                                "englishName": null,
-                                "icon": "",
-                                "noCache": false,
-                                "link": null
-                            },
-                            "platformId": 1
-                        },
-                        {
-                            "name": "Cascader",
-                            "path": "cascader",
-                            "hidden": false,
-                            "component": "project/yunboo/cascader/index",
-                            "meta": {
-                                "title": "级联选择器",
-                                "englishName": null,
-                                "icon": "",
-                                "noCache": false,
-                                "link": null
-                            },
-                            "platformId": 1
-                        },
-                    ],
-                    "platformId": 1
-                },
-            ],
-            "platformId": 1
-        },
-        {
-            "name": "Technology",
-            "path": "/technology",
-            "hidden": false,
-            "redirect": "noRedirect",
-            "component": "Layout",
-            "alwaysShow": true,
-            "meta": {
-                "title": "技术栈",
-                "englishName": "",
-                "icon": "energy-dn-houtai",
-                "noCache": false,
-                "link": null
-            },
-            "children": [
-                {
-                    "name": "HTML",
-                    "path": "HTML",
-                    "hidden": false,
-                    "redirect": "noRedirect",
-                    "component": "ParentView",
-                    "alwaysShow": true,
-                    "meta": {
-                        "title": "HTML",
-                        "englishName": null,
-                        "icon": "",
-                        "noCache": false,
-                        "link": null
-                    },
-                    "children": [
-                        {
-                            "name": "VNode",
-                            "path": "VNode",
-                            "hidden": false,
-                            "component": "technology/HTML/VNode/index",
-                            "meta": {
-                                "title": "虚拟节点",
-                                "englishName": null,
-                                "icon": "",
-                                "noCache": false,
-                                "link": null
-                            },
-                            "platformId": 1
-                        }
-                    ],
-                    "platformId": 1
-                },
-            ],
-            "platformId": 1
+          name: 'Home',
+          path: 'home',
+          hidden: false,
+          component: 'index',
+          meta: {
+            title: '首页',
+            englishName: '',
+            icon: 'energy-dn-kanban',
+            noCache: false,
+            link: null
+          },
+          platformId: 1
         }
-    ]
+      ],
+      platformId: 1
+    },
+    {
+      name: 'Project',
+      path: '/project',
+      hidden: false,
+      redirect: 'noRedirect',
+      component: 'Layout',
+      alwaysShow: true,
+      meta: {
+        title: '项目DEMO',
+        englishName: '',
+        icon: 'energy-dn-houtai',
+        noCache: false,
+        link: null
+      },
+      children: [
+        {
+          name: 'Yunboo',
+          path: 'yunboo',
+          hidden: false,
+          redirect: 'noRedirect',
+          component: 'ParentView',
+          alwaysShow: true,
+          meta: {
+            title: '鼋博',
+            englishName: null,
+            icon: '',
+            noCache: false,
+            link: null
+          },
+          children: [
+            {
+              name: 'MindMap',
+              path: 'mindMap',
+              hidden: false,
+              component: 'project/yunboo/mindMap/index',
+              meta: {
+                title: '脑图',
+                englishName: null,
+                icon: '',
+                noCache: false,
+                link: null
+              },
+              platformId: 1
+            },
+            {
+              name: 'GaussianDistribution',
+              path: 'gaussianDistribution',
+              hidden: false,
+              component: 'project/yunboo/gaussianDistribution/index',
+              meta: {
+                title: '正态分布',
+                englishName: null,
+                icon: '',
+                noCache: false,
+                link: null
+              },
+              platformId: 1
+            },
+            {
+              name: 'MassData',
+              path: 'massData',
+              hidden: false,
+              component: 'project/yunboo/massData/index',
+              meta: {
+                title: '表格大量数据',
+                englishName: null,
+                icon: '',
+                noCache: false,
+                link: null
+              },
+              platformId: 1
+            },
+            {
+              name: 'Cascader',
+              path: 'cascader',
+              hidden: false,
+              component: 'project/yunboo/cascader/index',
+              meta: {
+                title: '级联选择器',
+                englishName: null,
+                icon: '',
+                noCache: false,
+                link: null
+              },
+              platformId: 1
+            },
+            {
+              name: 'JsonEditor',
+              path: 'jsonEditor',
+              hidden: false,
+              component: 'project/yunboo/jsonEditor/index',
+              meta: {
+                title: 'json编辑器',
+                englishName: null,
+                icon: '',
+                noCache: false,
+                link: null
+              },
+              platformId: 1
+            }
+          ],
+          platformId: 1
+        }
+      ],
+      platformId: 1
+    },
+    {
+      name: 'Technology',
+      path: '/technology',
+      hidden: false,
+      redirect: 'noRedirect',
+      component: 'Layout',
+      alwaysShow: true,
+      meta: {
+        title: '技术栈',
+        englishName: '',
+        icon: 'energy-dn-houtai',
+        noCache: false,
+        link: null
+      },
+      children: [
+        {
+          name: 'HTML',
+          path: 'HTML',
+          hidden: false,
+          redirect: 'noRedirect',
+          component: 'ParentView',
+          alwaysShow: true,
+          meta: {
+            title: 'HTML',
+            englishName: null,
+            icon: '',
+            noCache: false,
+            link: null
+          },
+          children: [
+            {
+              name: 'VNode',
+              path: 'VNode',
+              hidden: false,
+              component: 'technology/HTML/VNode/index',
+              meta: {
+                title: '虚拟节点',
+                englishName: null,
+                icon: '',
+                noCache: false,
+                link: null
+              },
+              platformId: 1
+            }
+          ],
+          platformId: 1
+        }
+      ],
+      platformId: 1
+    }
+  ]
 }
---
+++ src/views/project/yunboo/jsonEditor/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <JsonEditorVue
+    style="height: 300px; width: 400px"
+    v-model="value"
+    v-bind="{
+      /* local props & attrs */
+      mode: 'text',
+      mainMenuBar: false,
+      statusBar: false,
+      navigationBar: false,
+      askToFormat: true
+    }"
+    @error="onError"
+  />
+</template>
+
+<script setup>
+import JsonEditorVue from 'json-editor-vue'
+
+const value = ref()
+
+const onError = (err) => {
+  console.log(err)
+}
+</script>
+
+<style scoped lang="scss"></style>
