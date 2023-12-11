3ac8bcc6d05a0019fb2c9337c32ae183a3e5dab8
chenzhang <245118190@qq.com>
2023-12-11 11:29:01 +0800

    add the function of search in cascader

--- src/views/project/yunboo/cascader/index.vue
+++ src/views/project/yunboo/cascader/index.vue
@@ -2,7 +2,7 @@
   <div style="padding: 16px">
     <div class="first__row" style="display: flex; margin-bottom: 16px">
       <el-input
-        v-model="input"
+        v-model="searchKeyword"
         placeholder="Please input"
         style="width: 240px; margin-right: 24px"
       />
@@ -10,12 +10,13 @@
     </div>
     <el-cascader-panel
       ref="cascaderRef"
-      v-model="panelValue"
+      v-model="panel"
       :options="options"
       :props="cascaderProps"
-      @change="change"
       style="width: 800px; background-color: #ffffff"
     />
+
+    <!--    <el-cascader></el-cascader>-->
   </div>
 </template>
 
@@ -268,6 +269,18 @@ const options = [
           {
             value: 'collapse',
             label: 'Collapse'
+          },
+          {
+            value: 'collapse1',
+            label: 'Collapse1'
+          },
+          {
+            value: 'collapse2',
+            label: 'Collapse2'
+          },
+          {
+            value: 'collapse3',
+            label: 'Collapse3'
           }
         ]
       }
@@ -293,64 +306,43 @@ const options = [
   }
 ]
 
-const panelValue = ref([])
+const panel = ref([])
 
 const cascaderRef = ref()
-const change = () => {
-  console.log('panelValue.value11', panelValue.value)
-}
-
 /**
  *@Author : ChenZhang
- *@Date : 2023/12/08 11:08:26
- *@Description : 添加过滤功能
+ *@Date : 2023/12/11 09:46:09
+ *@Description : 实现过滤功能
  */
-
-const currentSelect = ref()
-
-const currentSelectChange = () => {
-  console.log('currentSelect', currentSelect.value)
-  /**
-   *@Author : ChenZhang
-   *@Date : 2023/12/08 11:14:06
-   *@Description : 改变之后根据搜索的内容高亮当前的节点
-   */
-}
-
+const searchKeyword = ref('')
 const input = ref()
 
 //Component Form Checkbox
 const search = () => {
-  console.log(cascaderRef.value.menuList, 'menuList')
-  console.log(cascaderRef.value.menus, 'menus')
-  console.log(cascaderRef.value.checkedNodes, 'checkedNodes')
-  console.log(
-    'cascaderRef.value.menuList.menuId',
-    cascaderRef.value.menuList.menuId
-  )
+  console.log(panel.value, 'panel')
+  const { value } = searchKeyword
 
-  let el = document.querySelector(
-    `.el-cascader-node[aria-owns=${cascaderRef.value.menuList[0].menuId}]`
-  )
-  focusNode(el)
-  nextTick(() => {
-    //展开第二个节点
-    let el2 = document.querySelector(
-      `.el-cascader-node[aria-owns=${cascaderRef.value.menuList[1].menuId}]`
-    )
-    focusNode(el2)
-    nextTick(() => {
-      console.log(
-        'cascaderRef.value.menuList[2]',
-        cascaderRef.value.menuList[2].menuId
-      )
-      let menuId3 = cascaderRef.value.menuList[2].menuId
-      let uid3 = cascaderRef.value.menuList[2].nodes[0].uid
+  if (!value) return
 
-      let el3 = document.getElementById(`${menuId3}-${uid3}`)
-      focusNode(el3)
-      console.log(el3, 'el3')
-    })
+  const pathNodes = calculateSuggestions()
+
+  if (!pathNodes) {
+    return
+  }
+
+  let pathNodesLength = pathNodes.length
+
+  locateToNode(pathNodes, pathNodesLength)
+}
+
+const locateToNode = async (pathNodes, length, index = 0) => {
+  let menuId = cascaderRef.value.menuList[index].menuId
+  let uid = pathNodes[index].uid
+  nextTick(async () => {
+    let el = document.getElementById(`${menuId}-${uid}`)
+    await focusNode(el)
+    index++
+    if (index < length) await locateToNode(pathNodes, length, index)
   })
 }
 
@@ -366,4 +358,27 @@ const focusNode = (el) => {
   !isLeaf(el) && el.click()
 }
 const isLeaf = (el) => !el.getAttribute('aria-owns')
+
+const calculateSuggestions = () => {
+  /**
+   *@Author : ChenZhang
+   *@Date : 2023/12/11 10:10:39
+   *@Description : 精准匹配需要从查询的商品并且实现定位
+   */
+
+  const allNodes = cascaderRef.value.getFlattedNodes()
+  const targetNode = allNodes.find((item) => {
+    return item.level === 3 && item.label === searchKeyword.value
+  })
+
+  if (!targetNode) return
+  //返回一个pathNodes
+  return targetNode.pathNodes
+}
+
+/**
+ *@Author : ChenZhang
+ *@Date : 2023/12/11 11:12:55
+ *@Description : TODO 实现数据回显 数据的回显的就是找到对应的pathNodes 拼接路径或者是flattedNodes中的属性
+ */
 </script>
