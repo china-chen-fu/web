c936698a2efc94fd181a2e2aecda0332ada64761
chenzhang <245118190@qq.com>
2023-12-18 17:30:24 +0800

    test git push

--- hyboot-ui/src/api/payment/stallenergy.js
+++
@@ -1,9 +0,0 @@
-import request from "@/utils/request";
-
-export function getList(query) {
-    return request({
-      url: "/payment/stall/stallEnergySum",
-      method: "get",
-      params: query,
-    });
-  }
--- hyboot-ui/src/api/platform.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-// 获取系统模块
-export const getPlatforms = () => {
-  return request({
-    url: '/getPlatforms',
-    method: 'get'
-  })
-}
